2f0552a951cc97d6da8a573d6a192a0b722bb109
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-06-23 20:12:21 +0700

    Update line.js

--- src/app/components/line.js
+++ src/app/components/line.js
@@ -1,12 +1,14 @@
-import React from 'react'
+// components/Line.js
+import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
-export default function line() {
+
+export default function Line() {
   return (
     <div>
       <Link href='https://lin.ee/8Ty5gad' className=''>  
-<Image src="/img/line.png" width={50} height={50} alt="line" className='lines bounce' />
-</Link>
+        <Image src="/img/line.png" width={50} height={50} alt="line" className='lines bounce' />
+      </Link>
     </div>
-  )
+  );
 }
