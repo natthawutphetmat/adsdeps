f9788ac17d661ece6a49baacf463eadfce702325
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-06-23 20:15:54 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import "./modal.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/nav';
 import Footer from './components/Footer';
-import Line from './components/Line';
+import Lines from './components/Lines';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        <Line />
+        <Lines />
         {children}
         <Footer />
       </body>
