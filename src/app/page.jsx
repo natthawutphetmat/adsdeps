e4b262a5e26f7bc4420e9aba362d55175b5f88a9
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-06-23 21:58:07 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export const metadata = {
   title: "รับทำโฆษณาGoogleAdsสายเทา โฆษณาสายเทาเพื่อธุรกิจของคุณ",
   description: "รับทำโฆษณาออนไลน์สายเทา ผู้เชี่ยวชาญด้านการตลาดบน Google, YouTube และ Facebook",
   keywords: "ยิงads,facebook,สายเทา,โฆษณา,ยิงads facebook สายเทา,รับยิงads,รับยิงแอด สายเทา,facebook ads,google ads,google,รับยิงแอด,ads,รับทำโฆษณา,รับโฆษณา,Facebook,การตลาด,โฆษณาออนไลน์,เว็บไซต์,ตลาดเป้าหมาย,โฆษณาบนโซเชียลมีเดีย,Google Ads,การโฆษณาบน Facebook,การโฆษณาออนไลน์บนสื่อต่าง ๆ,การตลาดออนไลน์,การโปรโมท,โฆษณา Facebook,การโฆษณา Google,การโฆษณาสินค้า,การโฆษณาโปรโมชั่น,วิธีการโฆษณา,ความสำเร็จในการตลาด,บริการโฆษณา,การตลาดออนไลน์บน Facebook,การโฆษณาบนเว็บ,การวางแผนโฆษณา,รับจ้างโฆษณา",
-  image:"https://adsdeps.com/img/ads.jpg",
+  image:"https://www.adsdeps.com/img/ads.jpg",
   name: "adsmanager",
   canonical:'https://adsdeps.com',
 
